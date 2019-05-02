ddd1d89996e67a21c150a7bb9c11259c007bcff3
Raquel Castro <rcastro9@gmail.com>
2019-05-02 10:19:48 -0400

    Graphs Online Lab 34

---
+++ soliloquy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+#import csv
+
+open('sol.txt', 'r')
+
+
+#configfile = open(/home/student/"sol.txt", "r")
+
+## display file to the screen - .read()
+#configblog = configfile.read()
+
+## break configblog across line boundaries (strips out \n)
+#configlist = configblog.splitlines()
+
+## display list with no "\n"
+#print(configlist)
+
+## Always close your file
+#configfile.close()
